81b958a76a583cd16573d15c8520049f0fa6f5da
shubham mansute <shubhammansute7822@gmail.com>
2023-10-23 18:06:08 +0530

    removed index.js from schema folder

--- schema/index.js
+++
@@ -1,8 +0,0 @@
-const { ApolloServer } = require("apollo-server");
-const {typeDefs} = require("./type-defs")
-const {resolvers} = require("./resolvers")
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({ url }) => {
-    console.log(`YOUR API IS RUNNING AT: ${url} :)`);
-  });
